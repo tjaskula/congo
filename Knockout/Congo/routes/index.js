a18aa1151340a8e30f3f41df6c05adeb567b12a3
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-07-25 23:53:23 +0200

    Rename to Kngo

--- Knockout/Congo/routes/index.js
+++ Knockout/Congo/routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var index = express.Router();
 
 index.get('/', function(req, res, next){
-  res.render('index', { title: 'Congo: The Mongo Editor' });
+  res.render('index', { title: 'Kongo: The Mongo Editor' });
 });
 
 module.exports.index = index;
