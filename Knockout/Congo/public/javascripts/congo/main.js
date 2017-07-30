7112f4874a44fe364623eebe1ff0abc5c7af2ebf
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-07-30 02:25:12 +0200

    Add css to webpack configuration

---
+++ Knockout/Congo/public/javascripts/congo/main.js
@@ -0,0 +1,2 @@
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/dist/css/bootstrap-theme.min.css');
--- Knockout/Congo/webpack.config.js
+++ Knockout/Congo/webpack.config.js
@@ -10,7 +10,7 @@ function resolve(filePath) {
 module.exports = {
   devtool: 'source-map',
   entry: {
-    vendors: ['knockout', 'jquery', 'underscore', 'bootstrap']
+    vendors: ['./public/javascripts/congo/main.js', 'knockout', 'jquery', 'underscore', 'bootstrap']
   },
   output: {
     path: resolve('./public/javascripts'),
@@ -22,6 +22,15 @@ module.exports = {
       'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
     }
   },*/
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+    ]
+  },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendors',
