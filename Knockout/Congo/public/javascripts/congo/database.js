89be54e505be571f528768b2e834622fee525636
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-09-11 00:57:11 +0200

    displaying the list of databases

--- Knockout/Congo/public/javascripts/congo/database.js
+++ Knockout/Congo/public/javascripts/congo/database.js
@@ -5,31 +5,31 @@ Congo.databases =
     { id: 3, name: "three" }
   ];
 
-// Congo.databaseViewModel = function() {
-//   var view = new Congo.View("databaseView", "database-list-template", Congo.databases)
-//   tagName : "tr",
-//   template : "#database-list-template",
-//   events : {
-//     "click button" : "remove",
-//     "click a" : "showDb"
-//   },
-//   // showDb : function(ev) {
-//   //   ev.preventDefault();
-//   //   var db = $(ev.currentTarget).data("db");
-//   //   Congo.router.navigate(db, true);
-//   // }
-// };
+Congo.databaseViewModel = function(db) {
+  var view = new Congo.View("database-list-template", db)
+  return {
+    view : view
+  }
+  // events : {
+  //   "click button" : "remove",
+  //   "click a" : "showDb"
+  // },
+  // showDb : function(ev) {
+  //   ev.preventDefault();
+  //   var db = $(ev.currentTarget).data("db");
+  //   Congo.router.navigate(db, true);
+  // }
+};
 
 Congo.databaseListViewModel = function(databaseList){
-  var view = new Congo.View("database-list", databaseList)
-  //console.log(view);
+  var view = new Congo.View("database-list-template", databaseList)
   return {
     view : view
   }
 };
 
 Congo.databasesLayoutViewModel = function() {
-  var view = new Congo.View("db-details-template", Congo.databaseListViewModel(Congo.databases))
+  var view = new Congo.View("db-details-template", new Congo.databaseListViewModel(Congo.databases))
   return {
     view : view
   }
