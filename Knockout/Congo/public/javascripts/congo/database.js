bcee62b441e70b88141f2b84d005bb109099fdcf
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-09-19 23:49:36 +0200

    Adding option view model to database

--- Knockout/Congo/public/javascripts/congo/database.js
+++ Knockout/Congo/public/javascripts/congo/database.js
@@ -5,11 +5,11 @@ Congo.databases =
     { id: 3, name: "three" }
   ];
 
-Congo.databaseViewModel = function(db) {
-  var view = new Congo.View("database-list-template", db)
-  return {
-    view : view
-  }
+// Congo.databaseViewModel = function(db) {
+//   var view = new Congo.View("database-list-template", db)
+//   return {
+//     view : view
+//   }
   // events : {
   //   "click button" : "remove",
   //   "click a" : "showDb"
@@ -19,12 +19,14 @@ Congo.databaseViewModel = function(db) {
   //   var db = $(ev.currentTarget).data("db");
   //   Congo.router.navigate(db, true);
   // }
-};
+//};
 
 Congo.databaseListViewModel = function(databaseList){
-  var view = new Congo.View("database-list-template", databaseList)
+  var viewDetails = new Congo.View("database-list-template", databaseList)
+  var viewOptions = new Congo.View("new-db-template")
   return {
-    view : view
+    viewDetails : viewDetails,
+    viewOptions : viewOptions
   }
 };
 
